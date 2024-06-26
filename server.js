eb31cae06f04bb3a67a0067e55c16060fb0c67f5
sachin27verma <97831442+sachin27verma@users.noreply.github.com>
2024-06-27 00:54:49 +0530

    web socket server

---
+++ server.js
@@ -0,0 +1,51 @@
+import express from "express";
+import { Server } from "socket.io";
+import { createServer } from "http";
+import cors from "cors";
+
+const port = 8000;
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "https://healthcheak.vercel.app/", // Adjust to match the client URL and port
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
+app.use(
+  cors({
+    origin: "https://healthcheak.vercel.app/", // Adjust to match the client URL and port
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+io.on("connection", (socket) => {
+  console.log("User Connected", socket.id);
+
+  socket.on("message", ({ room, message }) => {
+    const messageData = { room, message, id: socket.id };
+    console.log(messageData);
+    socket.to(room).emit("receive-message", messageData);
+  });
+
+  socket.on("join-room", (room) => {
+    socket.join(room);
+    console.log(`User joined room ${room}`);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
